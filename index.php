b3dd1006d56989c545d3ab7c21968cdb36c66beb
Facundo Damián Fagnano <facundofagnano@gmail.com>
2022-10-21 00:36:03 -0300

    Base
    Base del trabajo. Cada html hecho y separado

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once("header.html");
+include_once("revista.html");
+include_once ("content.html");
+include_once("login.html");
+include_once("registry.html");
+include_once("footer.html");
+
